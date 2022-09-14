84a02227b28e1f0a6fd5941f3ea4a8583056ea05
mrunal <mrunalbhoir100@gmail.com>
2022-09-14 20:11:38 +0530

    Create a Rest Controller

--- src/main/java/com/example/spring/restController.java
+++ src/main/java/com/example/spring/restController.java
@@ -1,17 +1,17 @@
 package com.example.spring;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 
 public class restController {
-    @RequestMapping
+    @RequestMapping("/hello")
 
     public String HelloWorld(){
-        return "hello world spring";
+        return "hello from bridgelabz";
     }
+
+
     }
 
