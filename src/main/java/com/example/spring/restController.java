68a7db5105535b7967c824802ec657d0fdbfd1dd
mrunal <mrunalbhoir100@gmail.com>
2022-09-14 21:14:31 +0530

    uc2 Make REST Call to show Hello Mark from BridgeLabz

--- src/main/java/com/example/spring/restController.java
+++ src/main/java/com/example/spring/restController.java
@@ -6,12 +6,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 
 public class restController {
-    @RequestMapping("/hello")
-
-    public String HelloWorld(){
-        return "hello from bridgelabz";
+//    @RequestMapping("/hello")
+//
+//    public String HelloWorld(){
+//        return "hello from bridgelabz";
+//    }
+    /*using-http://localhost:8081/hello/query?name= mrunal*/
+    @RequestMapping(value = {"/query"},method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name")String Name){
+        return "Hello " + Name+ " from BridgeLabs.";
     }
-
+//    @RequestMapping("/param/{name}")
+//    public String sayHelloParam(@PathVariable String name){
+//        return "Hello"+name+ " from BridgeLabs.";
+//    }
 
     }
 
